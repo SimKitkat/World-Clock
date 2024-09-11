00a1ed0f8dfc7b9bf02507f1ea7fd943a65db13d
SimKitkat <simone.cornelius14@gmail.com>
2024-09-11 15:36:40 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -9,14 +9,32 @@ function updateTime() {
 
   let RomeElement = document.querySelector("#Rome");
 
-  let RomeDateElement = Rome.querySelector(".date");
-  let RomeTimeElement = Rome.querySelector(".time");
+  let RomeDateElement = RomeElement.querySelector(".date");
+  let RomeTimeElement = RomeElement.querySelector(".time");
   let RomeTime = moment().tz("Europe/Rome");
 
   RomeDateElement.innerHTML = RomeTime.format("MMMM	Do YYYY");
-  RomeTimeElement.innerHTML = RomeTime.format(
-    "h:mm:ss" + "   " + "[<small>]A[</small>]"
-  );
+  RomeTimeElement.innerHTML = RomeTime.format("h:mm:ss [<small>]A[</small>]");
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = ` 
+  <div class="city" id="cairo">
+          <div class="name-date">
+            <h2>${cityName}🕛</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")}</div>
+        </div>
+         
+        `;
 }
 
 setInterval(updateTime, 1000);
+updateCity();
+
+let citiesSelectElement = document.querySelector("#cityoptions");
+citiesSelectElement.addEventListener("change", updateCity);
