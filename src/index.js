580d78cbdb3b54f2047423d289d7909f7479eafb
SimKitkat <simone.cornelius14@gmail.com>
2024-09-12 14:26:49 +0200

    all cities link added

--- src/index.js
+++ src/index.js
@@ -48,7 +48,10 @@ function updateCity(event) {
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
+
   </div>
+    <a href="index.html" class="all-cities-link">All cities</a>
+  
   `;
 }
 updateTime();
